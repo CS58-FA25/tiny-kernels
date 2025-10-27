0319eca0a3ff76f10f7c0a8793b5a360169c443f
bfu4 <bella.lily.fusari@dartmouth.edu>
2025-10-27 17:10:47 -0400

    (add): demo code for clock.c

--- src/traps/clock.c
+++ src/traps/clock.c
@@ -1,13 +1,20 @@
 #include "trap.h"
 
+#include <hardware.h> // UserContext
+
+/**
+ * TODO:
+ * necessary duration of time has passed
+ *
+ * update processes, timers of the time change
+ * update scheduler of the time change
+ * ^
+ * |_____ these updates will handle the actions on "time passed"
+ *
+ * context does not need to be modified for this
+ */
 void ClockTrapHandler(UserContext* ctx) {
-   // necessary duration of time has passed
-   //
-   // update processes, timers of the time change
-   // update scheduler of the time change
-   // ^
-   // |_____ these updates will handle the actions on "time passed"
-   // 
-   // context does not need to be modified for this
+   // Checkpoint 2: Temporary code
+   TracePrintf(TRAP_TRACE_LEVEL, "[CLOCK_TRAP] Clock trap triggered\n")
 }
 
